8ab3093ee7b42224c40d323056a7db1482ede896
Vyekk <s20699@pjwstk.edu.pl>
2023-04-16 22:14:12 +0200

    Prepared root file

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WebsiteView from "views/WebsiteView/WebsiteView";
 import styles from "views/App/App.module.scss";
 
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+class App extends React.Component {
+    state = {
+    
+  }
+  render() {
+    return (
+      <>
+        <h1>Hello world!</h1>
+        <BrowserRouter>
+        <>
+          <Routes>
+            <Route path="/" element={<WebsiteView />}></Route>
+          </Routes>
+        </>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
