5fdc0c25fb3b6dcc3dc14071e795bba6098bff71
Vyekk <s20699@pjwstk.edu.pl>
2023-04-18 15:24:45 +0200

    Added first veresion of welcome-page

--- src/views/WebsiteView/WebsiteView.js
+++ src/views/WebsiteView/WebsiteView.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Section from "components/Section/Section";
+import Title from "components/Title/Title";
+import headerImage from "assets/header_image.png";
+import profitsImage from "assets/profits.png";
+import Footer from "components/Footer/Footer";
+import Header from "components/Header/Header";
 
 const WebsiteView = () => (
     <>
-        <h1>Webiste</h1>
+        <Section url={headerImage}>
+          <Header />
+          <Title tag="h2" orange>Hello there</Title>
+        </Section>
+        <Section id="about" dark>
+
+        </Section>
+        <Section id="demo">
+
+        </Section>
+        <Section id="profits" url={profitsImage}>
+
+        </Section>
+        <Section id="contact" dark>
+
+        </Section>
+        <Footer />
     </>
 );
 
