e3f02c6126efb023c343d4ff2a37a17a1240ef43
Vyekk <s20699@pjwstk.edu.pl>
2023-04-18 15:22:39 +0200

    Added component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "components/Footer/Footer.module.scss";
+
+const Footer = () => (
+    <footer className={styles.wrapper}>
+        <p>Copyright &copy; by Konrad Koluch</p>
+    </footer>
+);
+
+export default Footer;
