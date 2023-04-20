113fed228b43a07f1e110ec650072b2323a121b4
Vyekk <s20699@pjwstk.edu.pl>
2023-04-20 03:31:46 +0200

    Added mobile version of nav menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,30 @@ import React from "react";
 import styles from "components/Header/Header.module.scss";
 import HeaderNavigation from "components/Header/HeaderNavigation";
 import Logo from "components/Logo/Logo";
-const Header = () => (
-    <header className={styles.wrapper}>
-        <Logo />
-        <HeaderNavigation />
-    </header>
-);
+import Button from "components/Button/Button";
+
+class Header extends React.Component {
+    state = {
+        isVisible: false,
+    }
+    changeVisibility = () => {
+        if(this.state.isVisible == true) {
+            this.setState({isVisible: false});
+        } else {
+            this.setState({isVisible: true});
+        }
+    }
+    render() {
+        return (
+            <header className={styles.wrapper}>
+                <div className={styles.logoNav}>
+                    <Logo />
+                    <Button onClick={this.changeVisibility}>&equiv;</Button>
+                </div>
+                <HeaderNavigation visible={this.state.isVisible} />
+            </header>
+        );
+    }
+}
 
 export default Header;
--- src/components/Header/HeaderNavigation.js
+++ src/components/Header/HeaderNavigation.js
@@ -3,15 +3,18 @@ import {HashLink} from 'react-router-hash-link';
 import styles from "components/Header/HeaderNavigation.module.scss";
 
 
-const HeaderNavigation = () => (
-    <nav className={styles.wrapper}>
-        <ul className={styles.navList}>
-            <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#about">O projekcie</HashLink></li>
-            <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#demo">Demo</HashLink></li>
-            <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#profits">Zalety</HashLink></li>
-            <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#contact">Kontakt</HashLink></li>
-        </ul>
-    </nav>
-);
+const HeaderNavigation = ({visible}) => {
+    const wrapperStyle = visible ? styles.wrapper : styles.wrapperHide;
+    return (
+        <nav className={wrapperStyle}>
+            <ul className={styles.navList}>
+                <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#about">O projekcie</HashLink></li>
+                <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#demo">Demo</HashLink></li>
+                <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#profits">Zalety</HashLink></li>
+                <li className={styles.navListItem}><HashLink className={styles.hashLink} to="/#contact">Kontakt</HashLink></li>
+            </ul>
+        </nav>
+    );
+}
 
 export default HeaderNavigation;
