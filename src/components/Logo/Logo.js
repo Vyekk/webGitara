22c3b21125b7394d32e683e6d551d7b279134e61
Vyekk <s20699@pjwstk.edu.pl>
2023-04-20 03:33:32 +0200

    Changed logo structure and linked

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "components/Logo/Logo.module.scss";
+import { Link } from "react-router-dom";
 
 const Logo = () => (
-    <p className={styles.logo}>web<span className={styles.logoPrimary}>Gitara</span></p>
+    <p className={styles.logo}>
+        <Link to="/">
+            web<span className={styles.logoPrimary}>Gitara</span>
+        </Link>
+    </p>
 );
 
 export default Logo;
