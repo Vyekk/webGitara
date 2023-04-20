13aedaf3618a485783f379bc877e99b4dbbcb080
Vyekk <s20699@pjwstk.edu.pl>
2023-04-20 02:09:58 +0200

    Added new route path

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WebsiteView from "views/WebsiteView/WebsiteView";
+import LoginView from "views/LoginView/LoginView";
 import styles from "views/App/App.module.scss";
 
 class App extends React.Component {
@@ -10,11 +11,11 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>Hello world!</h1>
         <BrowserRouter>
         <>
           <Routes>
             <Route path="/" element={<WebsiteView />}></Route>
+            <Route path="/login" element={<LoginView />}></Route>
           </Routes>
         </>
         </BrowserRouter>
