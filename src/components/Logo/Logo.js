b6fa66554f4fd59e652d5e7994fcb436328b62bc
Vyekk <s20699@pjwstk.edu.pl>
2023-04-20 02:00:49 +0200

    Added Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "components/Logo/Logo.module.scss";
+
+const Logo = () => (
+    <p className={styles.logo}>web<span className={styles.logoPrimary}>Gitara</span></p>
+);
+
+export default Logo;
