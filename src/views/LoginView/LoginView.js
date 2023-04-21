f824161f08f872940262ad9131c5533e8f6fcd6f
Vyekk <s20699@pjwstk.edu.pl>
2023-04-21 19:15:01 +0200

    Changed component to class component

--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -1,16 +1,25 @@
 import React from "react";
-import Title from "components/Title/Title";
+import { Link } from "react-router-dom";
 import Logo from "components/Logo/Logo";
 import Section from "components/Section/Section";
 import loginPageImage from "assets/login-page-background.png";
 import styles from "views/LoginView/LoginView.module.scss";
+import Modal from "components/Modal/Modal";
 
-const LoginView = () => (
-    <Section url={loginPageImage}>
-        <div className={styles.wrapper}>
-            <Logo />
-        </div>
-    </Section>
-);
+class LoginView extends React.Component {
+    render() {
+        return (
+            <Section url={loginPageImage}>
+                <div className={styles.wrapper}>
+                    <Logo />
+                    <Link to="/">&lt; Wróć do strony głównej</Link>
+                    <div className={styles.loginRegisterWrapper}>
+                        <Modal></Modal>
+                    </div>
+                </div>
+            </Section>
+        );
+    }
+}
 
 export default LoginView;
