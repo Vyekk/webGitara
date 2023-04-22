9da105d6656070e02968a57b885474c13fd2c364
Vyekk <s20699@pjwstk.edu.pl>
2023-04-22 04:47:44 +0200

    Added default props type: text

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "components/Input/Input.module.scss";
+import PropTypes from "prop-types";
 
-const Input = ({label}) => (
+const Input = ({children, id, type}) => (
     <div className={styles.wrapper}>
-        <input type="text" className={styles.input} placeholder={label} id={label} required />
+        <input type={type} className={styles.input} placeholder={children} id={id} required />
     </div>
 );
+
+Input.propTypes = {
+    type: PropTypes.string,
+};
+Input.defaultProps = {
+    type: "text",
+};
 
 export default Input;
