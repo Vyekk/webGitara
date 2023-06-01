28f18a48e904805fec0a335e9e0a8c59530594fb
Vyekk <s20699@pjwstk.edu.pl>
2023-06-01 17:12:22 +0200

    Added first changes to play view

--- src/views/PlayView/PlayView.tsx
+++ src/views/PlayView/PlayView.tsx
@@ -1,5 +1,16 @@
+import Section from 'components/Section/Section';
+import styles from 'views/PlayView/PlayView.module.scss';
+import { Route, Routes } from 'react-router-dom';
+import DashboardView from 'views/DashboardView/DashboardView';
+
 const PlayView = () => {
-    return <></>;
+    return (
+        <Section id={styles.playBackground}>
+            <Routes>
+                <Route path="dashboard" element={<DashboardView />}></Route>
+            </Routes>
+        </Section>
+    );
 };
 
 export default PlayView;
