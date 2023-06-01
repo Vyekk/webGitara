2045c8ea76ad459c83ed136ba595feb62b519af3
Vyekk <s20699@pjwstk.edu.pl>
2023-06-01 17:15:16 +0200

    Changed propTypes to ts

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,23 +1,15 @@
 import styles from 'components/Input/Input.module.scss';
-import PropTypes from 'prop-types';
 
 interface IInputProps {
     children: string;
     id: string;
-    type: string;
+    type?: string;
 }
 
-const Input = ({ children, id, type }: IInputProps) => (
+const Input = ({ children, id, type = 'text' }: IInputProps) => (
     <div className={styles.wrapper}>
         <input type={type} className={styles.input} placeholder={children} id={id} required />
     </div>
 );
-
-Input.propTypes = {
-    type: PropTypes.string,
-};
-Input.defaultProps = {
-    type: 'text',
-};
 
 export default Input;
