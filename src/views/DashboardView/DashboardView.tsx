f1577259951cca9dac5c712ee6be1bb15355a124
Vyekk <s20699@pjwstk.edu.pl>
2023-07-18 22:40:14 +0200

    Added class wrapper

--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -1,7 +1,16 @@
+import Logo from 'components/Logo/Logo';
 import Toolbar from 'components/Toolbar/Toolbar';
+import styles from 'views/DashboardView/Dashboard.module.scss';
 
 const DashboardView = () => {
-    return <Toolbar />;
+    return (
+        <>
+            <Toolbar />
+            <div className={styles.wrapper}>
+                <Logo />
+            </div>
+        </>
+    );
 };
 
 export default DashboardView;
