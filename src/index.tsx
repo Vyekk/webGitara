e3386dbdbccc6430986965e25b3cc8f9aa47da9f
Vyekk <koluchkon@gmail.com>
2024-05-13 16:45:55 +0200

    Changed render in react

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'index.scss';
 import App from 'views/App/App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root'),
 );
