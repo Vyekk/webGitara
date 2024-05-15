72358123f2220329dbabc19a89143fdb1db86a12
Vyekk <koluchkon@gmail.com>
2024-05-15 19:28:07 +0200

    Added info about success registration

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -12,60 +12,77 @@ const types = {
 
 const Form = ({ submitFn }: any) => {
     const [activeOption, setActiveOption] = useState(types.login);
+    const [isRegistered, setIsRegistered] = useState(false);
+
+    const handleRegister = () => {
+        // Tutaj powinna być logika rejestracji, ale na razie ustawiamy stan na true
+        setIsRegistered(true);
+    };
 
     return (
         <div className={styles.wrapper}>
-            {activeOption === types.login ? <Title>Zaloguj się</Title> : <Title>Zarejestruj się</Title>}
-            {activeOption === types.login ? <p>i kontynuuj praktykę</p> : <p>i wkrocz do świata muzyki</p>}
-            <form className={styles.form} onSubmit={submitFn}>
-                <div className={styles.radioWrapper}>
-                    <Radio
-                        name="formType"
-                        value={types.login}
-                        checked={activeOption === types.login}
-                        onChange={() => setActiveOption(types.login)}
-                    />
-                    <Radio
-                        name="formType"
-                        value={types.register}
-                        checked={activeOption === types.register}
-                        onChange={() => setActiveOption(types.register)}
-                    />
-                </div>
-                <div className={styles.inputWrapper}>
-                    <div className={styles.loginPasswordWrapper}>
-                        <Input id="login">Login</Input>
-                        <Input id="password" type="password">
-                            Hasło
-                        </Input>
-                        {activeOption !== types.login ? (
-                            <>
-                                <Input id="rePassword" type="password">
-                                    Powtórz hasło
-                                </Input>
-                                <Input id="email" type="email">
-                                    E-mail
-                                </Input>
-                            </>
-                        ) : null}
-                    </div>
-                    {activeOption !== types.login ? (
-                        <div className={styles.approvalWrapper}>
-                            <input id="statute" type="checkbox" />
-                            <label htmlFor="statute">
-                                Akceptuję regulamin platformy i zobowiązuje się do jego przestrzegania
-                            </label>
-                            <br />
-                            <input id="privacyPolicy" type="checkbox" />
-                            <label htmlFor="privacyPolicy">
-                                Akceptuję politykę prywatności platformy i wyrażam zgodę na przetwarzanie moich danych
-                                osobowych zgodnie z jej postanowieniami.
-                            </label>
+            {isRegistered ? (
+                <>
+                    <p>Konto założone pomyślnie</p>
+                    <button onClick={() => setIsRegistered(false)}>Wróć do logowania</button>
+                </>
+            ) : (
+                <>
+                    {activeOption === types.login ? <Title>Zaloguj się</Title> : <Title>Zarejestruj się</Title>}
+                    {activeOption === types.login ? <p>i kontynuuj praktykę</p> : <p>i wkrocz do świata muzyki</p>}
+                    <form className={styles.form} onSubmit={submitFn}>
+                        <div className={styles.radioWrapper}>
+                            <Radio
+                                name="formType"
+                                value={types.login}
+                                checked={activeOption === types.login}
+                                onChange={() => setActiveOption(types.login)}
+                            />
+                            <Radio
+                                name="formType"
+                                value={types.register}
+                                checked={activeOption === types.register}
+                                onChange={() => setActiveOption(types.register)}
+                            />
                         </div>
-                    ) : null}
-                </div>
-                <Button type="submit">{activeOption === types.login ? 'Zaloguj' : 'Zarejestruj'}</Button>
-            </form>
+                        <div className={styles.inputWrapper}>
+                            <div className={styles.loginPasswordWrapper}>
+                                <Input id="login">Login</Input>
+                                <Input id="password" type="password">
+                                    Hasło
+                                </Input>
+                                {activeOption !== types.login ? (
+                                    <>
+                                        <Input id="rePassword" type="password">
+                                            Powtórz hasło
+                                        </Input>
+                                        <Input id="email" type="email">
+                                            E-mail
+                                        </Input>
+                                    </>
+                                ) : null}
+                            </div>
+                            {activeOption !== types.login ? (
+                                <div className={styles.approvalWrapper}>
+                                    <input id="statute" type="checkbox" />
+                                    <label htmlFor="statute">
+                                        Akceptuję regulamin platformy i zobowiązuje się do jego przestrzegania
+                                    </label>
+                                    <br />
+                                    <input id="privacyPolicy" type="checkbox" />
+                                    <label htmlFor="privacyPolicy">
+                                        Akceptuję politykę prywatności platformy i wyrażam zgodę na przetwarzanie moich
+                                        danych osobowych zgodnie z jej postanowieniami.
+                                    </label>
+                                </div>
+                            ) : null}
+                        </div>
+                        <Button type="submit" onClick={handleRegister}>
+                            {activeOption === types.login ? 'Zaloguj' : 'Zarejestruj'}
+                        </Button>
+                    </form>
+                </>
+            )}
         </div>
     );
 };
