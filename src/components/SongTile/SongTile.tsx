209954a145e6dd47e9fa64bf4cb94fe98ff3c730
Vyekk <s20699@pjwstk.edu.pl>
2024-06-16 22:24:25 +0200

    Changed song tile for more customization

--- src/components/SongTile/SongTile.tsx
+++ src/components/SongTile/SongTile.tsx
@@ -3,16 +3,18 @@ import styles from 'components/SongTile/SongTile.module.scss';
 import Title from 'components/Title/Title';
 
 interface SongTileProps {
+    songTitle: string;
+    author: string;
     place?: number;
     rating?: number;
 }
 
-const SongTile = ({ place, rating }: SongTileProps) => {
+const SongTile = ({ songTitle, author, rating, place }: SongTileProps) => {
     return (
         <div className={styles.songTile}>
             <Rating rating={rating} />
-            <Title tag="h3">Tytuł utworu</Title>
-            <div className={styles.songAuthor}>Autor utworu</div>
+            <Title tag="h3">{songTitle}</Title>
+            <div className={styles.songAuthor}>{author}</div>
             {place === 1 && <div className={`${styles.songTop} ${styles.songTopGold}`}>{place}</div>}
             {place === 2 && <div className={`${styles.songTop} ${styles.songTopSilver}`}>{place}</div>}
             {place === 3 && <div className={`${styles.songTop} ${styles.songTopBronze}`}>{place}</div>}
