ddc57f6eb7043b70c9cce8ecccc84898c5dc57f7
Vyekk <s20699@pjwstk.edu.pl>
2024-06-16 22:23:48 +0200

    Added songList first changes and styles

--- src/components/SongsList/SongsList.tsx
+++ src/components/SongsList/SongsList.tsx
@@ -1,7 +1,24 @@
-const SongsList = () => {
+import styles from 'components/SongsList/SongsList.module.scss';
+import { SongTile } from 'components/SongTile/SongTile';
+import { Song } from 'types';
+
+type SongsListProps = {
+    isVertical: boolean;
+    songs: Song[];
+};
+
+const SongsList = ({ isVertical, songs }: SongsListProps) => {
     return (
-        <div>
-            <h1>Songs List</h1>
+        <div className={isVertical ? styles.verticalList : styles.horizontalList}>
+            {songs.map((song, index) => (
+                <SongTile
+                    key={index}
+                    songTitle={song.songTitle}
+                    author={song.author}
+                    rating={song.rating}
+                    place={song.place}
+                />
+            ))}
         </div>
     );
 };
