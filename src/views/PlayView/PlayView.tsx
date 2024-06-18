0af29c9daca5372ef961c5cdaf534fd33b1feb1b
Vyekk <koluchkon@gmail.com>
2024-06-18 23:36:09 +0200

    Added route for guitar view

--- src/views/PlayView/PlayView.tsx
+++ src/views/PlayView/PlayView.tsx
@@ -2,12 +2,14 @@ import Section from 'components/Section/Section';
 import styles from 'views/PlayView/PlayView.module.scss';
 import { Route, Routes } from 'react-router-dom';
 import DashboardView from 'views/DashboardView/DashboardView';
+import { GuitarView } from 'views/GuitarView/GuitarView';
 
 const PlayView = () => {
     return (
         <Section id={styles.playBackground}>
             <Routes>
                 <Route path="dashboard" element={<DashboardView />}></Route>
+                <Route path="guitar" element={<GuitarView />}></Route>
             </Routes>
         </Section>
     );
