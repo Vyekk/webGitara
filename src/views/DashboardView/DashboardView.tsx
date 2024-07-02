a85688ed5f26ad47b0f6d0dc7d7757de5f30dae5
Vyekk <s20699@pjwstk.edu.pl>
2024-07-02 16:36:53 +0200

    Changed example data (rating to array)

--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -7,9 +7,9 @@ import styles from 'views/DashboardView/Dashboard.module.scss';
 
 const DashboardView = () => {
     const songsListTest = [
-        { songTitle: 'Hey Jude', author: 'The Beatles', rating: 4.5, place: 1 },
-        { songTitle: 'Stairway to Heaven', author: 'Led Zeppelin', rating: 4.3, place: 2 },
-        { songTitle: 'Hotel California', author: 'Eagles', rating: 4.2, place: 3, liked: true },
+        { songTitle: 'Hey Jude', author: 'The Beatles', rating: [1, 2, 3, 4, 5, 4, 5, 5], place: 1 },
+        { songTitle: 'Stairway to Heaven', author: 'Led Zeppelin', rating: [1, 1, 2, 2, 2, 5], place: 2 },
+        { songTitle: 'Hotel California', author: 'Eagles', rating: [4, 4, 5, 5, 5], place: 3, liked: true },
     ];
     return (
         <>
