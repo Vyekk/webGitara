4402a7c632b49de9724df54b9c7658a9e5a5a08a
Vyekk <s20699@pjwstk.edu.pl>
2024-07-02 14:56:47 +0200

    Added like option

--- src/components/SongTile/SongTile.tsx
+++ src/components/SongTile/SongTile.tsx
@@ -1,17 +1,34 @@
 import { Rating } from 'components/Rating/Rating';
 import styles from 'components/SongTile/SongTile.module.scss';
 import Title from 'components/Title/Title';
+import { useRef, useState } from 'react';
 
 interface SongTileProps {
     songTitle: string;
     author: string;
     place?: number;
     rating?: number;
+    liked?: boolean;
 }
 
-const SongTile = ({ songTitle, author, rating, place }: SongTileProps) => {
+const SongTile = ({ songTitle, author, rating, place, liked }: SongTileProps) => {
+    const [songLiked, setSongLiked] = useState(liked);
+    const likedRef = useRef<HTMLDivElement>(null);
+    const handleMouseOver = () => {
+        likedRef.current?.classList.add(styles.likedHover);
+    };
+
+    const handleMouseOut = () => {
+        likedRef.current?.classList.remove(styles.likedHover);
+    };
+
+    const handleLikeClick = () => {
+        setSongLiked((prevState) => !prevState);
+    };
+
     return (
-        <div className={styles.songTile}>
+        <div className={styles.songTile} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            <div className={songLiked ? styles.liked : styles.unliked} ref={likedRef} onClick={handleLikeClick}></div>
             <Rating rating={rating} />
             <Title tag="h3">{songTitle}</Title>
             <div className={styles.songAuthor}>{author}</div>
