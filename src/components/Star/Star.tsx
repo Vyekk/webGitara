c141c039418584c836a92887ae6d12058d188b32
Vyekk <koluchkon@gmail.com>
2024-07-06 22:51:59 +0200

    Changed stars for rating

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import styles from './Star.module.scss';
 
 interface StarProps {
     fill?: 'full' | 'half' | 'none';
+    rating: number;
+    rateSong: (rate: number) => void;
 }
 
-const Star = ({ fill = 'none' }: StarProps) => <div className={`${styles.star} ${styles[fill]}`} />;
+const Star = ({ fill = 'none', rating, rateSong }: StarProps) => {
+    const [songRating, setSongRating] = useState(rating);
+    <div className={`${styles.star} ${styles[fill]}`} onClick={() => rateSong(songRating)} />;
+};
 
 export { Star };
