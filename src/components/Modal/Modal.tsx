304e9b7541e2ac6b036393938c2b31f638eb270c
Vyekk <s20699@pjwstk.edu.pl>
2024-07-11 23:46:04 +0200

    Added option to close modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,8 +3,14 @@ import styles from 'components/Modal/Modal.module.scss';
 
 interface IModalProps {
     children: ReactNode;
+    onClose: () => void;
 }
 
-const Modal = ({ children }: IModalProps) => <div className={styles.wrapper}>{children}</div>;
+const Modal = ({ children, onClose }: IModalProps) => (
+    <div className={styles.wrapper}>
+        {children}
+        <button onClick={onClose}>Close Modal</button>
+    </div>
+);
 
 export default Modal;
