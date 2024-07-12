7ab2f2daa9571eadbafc0b5c00d302bc694bd7a3
Vyekk <s20699@pjwstk.edu.pl>
2024-07-12 04:44:31 +0200

    Changes for modal content

--- src/components/Modal/ModalContext.tsx
+++ src/components/Modal/ModalContext.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { ReactNode, createContext } from 'react';
 
-export const ModalContext = React.createContext({
+type ModalContextType = {
+    modalContent: ReactNode | null;
+    isModalOpen: boolean;
+    openModal: () => void;
+    closeModal: () => void;
+    setModal: (content: ReactNode) => void;
+};
+
+export const ModalContext = createContext<ModalContextType>({
+    modalContent: null,
     isModalOpen: false,
-    openModal: () => {
-        console.log('openModal');
-    },
-    closeModal: () => {
-        console.log('closeModal');
-    },
+    openModal: () => {},
+    closeModal: () => {},
+    setModal: () => {},
 });
--- src/components/Modal/ModalProvider.tsx
+++ src/components/Modal/ModalProvider.tsx
@@ -5,11 +5,19 @@ import { ReactNode } from 'react';
 
 const ModalProvider = ({ children }: { children: ReactNode }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState<ReactNode>();
 
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
+    const setModal = (content: ReactNode) => {
+        setModalContent(content);
+    };
 
-    return <ModalContext.Provider value={{ isModalOpen, openModal, closeModal }}>{children}</ModalContext.Provider>;
+    return (
+        <ModalContext.Provider value={{ modalContent, isModalOpen, openModal, closeModal, setModal }}>
+            {children}
+        </ModalContext.Provider>
+    );
 };
 
 export default ModalProvider;
