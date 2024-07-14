f0e4899dd178ad5a47a78cec9380225917a2c29a
Vyekk <koluchkon@gmail.com>
2024-07-14 23:39:43 +0200

    Added Create new song option

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -42,13 +42,21 @@ const Toolbar = ({ hasControls }: ToolbarProps) => {
         setModal(content);
         openModal();
     };
+    const handleSettingsButtonClick = () => {
+        const content = <div>Settings</div>;
+        setModal(content);
+        openModal();
+    };
     return (
         <div className={styles.wrapper}>
             {hasControls && <Button isDark>BPM</Button>}
+            <Button className={styles.secondOption} href="../new" transparent>
+                Nowy utwór
+            </Button>
             <Button className={styles.playButton} onClick={handlePlayButtonClick} circle>
                 <img src={playIcon} alt="play" />
             </Button>
-            <Button className={styles.secondOption} href="/login" transparent>
+            <Button className={styles.secondOption} onClick={handleSettingsButtonClick} transparent>
                 <img className={styles.settingsIcon} src={settingsIcon} alt="settings" />
             </Button>
             <Button className={styles.secondOption} href="/login" transparent>
