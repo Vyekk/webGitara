2f820f30ce91ac2c9788115d834fe84426eea98d
Vyekk <s20699@pjwstk.edu.pl>
2024-07-28 04:03:09 +0200

    Added functionality to search song

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -6,6 +6,7 @@ import Input from 'components/Input/Input';
 
 export const MainMenu = () => {
     const [buttonType, setButtonType] = useState('allSongs');
+    const [searchTerm, setSearchTerm] = useState('');
     const songsListTest = [
         { songTitle: 'Hey Jude', author: 'The Beatles', rating: [1, 2, 3, 4, 5, 4, 5, 5], place: 4 },
         { songTitle: 'Stairway to Heaven', author: 'Led Zeppelin', rating: [1, 1, 2, 2, 2, 5], place: 6 },
@@ -20,6 +21,10 @@ export const MainMenu = () => {
         { songTitle: 'Stairway to Heaven', author: 'Led Zeppelin', rating: [1, 1, 2, 2, 2, 5], place: 6 },
         { songTitle: 'Hotel California', author: 'Eagles', rating: [4, 4, 5, 5, 5], place: 9, liked: true },
     ];
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm);
+    };
 
     return (
         <div>
@@ -27,13 +32,17 @@ export const MainMenu = () => {
                 <Button transparent onClick={() => setButtonType('mySongs')}>
                     Moje utwory
                 </Button>
-                {buttonType === 'allSongs' && <Input id="search">Wyszukaj utwór</Input>}
+                {buttonType === 'allSongs' && (
+                    <Input id="search" onChange={handleSearch}>
+                        Wyszukaj utwór
+                    </Input>
+                )}
                 <Button transparent onClick={() => setButtonType('allSongs')}>
                     Wszystkie utwory
                 </Button>
             </div>
             <div className={styles.songsWrapper}>
-                {buttonType === 'allSongs' ? <SongsList songs={songsListTest} /> : null}
+                {buttonType === 'allSongs' ? <SongsList songs={songsListTest} searchTerm={searchTerm} /> : null}
             </div>
         </div>
     );
