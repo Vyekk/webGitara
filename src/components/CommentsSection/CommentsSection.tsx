835332409d1af27914fc09a7e9c817687a00bde0
Vyekk <koluchkon@gmail.com>
2024-08-02 23:41:56 +0200

    Added file for comments section

---
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -0,0 +1,7 @@
+import styles from 'components/CommentsSection/CommentsSection.module.scss';
+
+const CommentsSection = () => {
+    return <div className={styles.commentsSectionWraper}></div>;
+};
+
+export { CommentsSection };
