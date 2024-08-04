9bfceb254016b7801d51650f36d567972451bd7d
Vyekk <koluchkon@gmail.com>
2024-08-04 23:54:38 +0200

    Added body of comment section

--- src/components/CommentsSection/CommentsSection.tsx
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -1,7 +1,26 @@
+import Button from 'components/Button/Button';
 import styles from 'components/CommentsSection/CommentsSection.module.scss';
 
-const CommentsSection = () => {
-    return <div className={styles.commentsSectionWraper}></div>;
+type CommentsSectionProps = {
+    song: object;
+};
+
+const CommentsSection = (song: CommentsSectionProps) => {
+    const addComment = () => {
+        console.log('Dodaj komentarz');
+    };
+
+    return (
+        <div className={styles.commentsSectionWraper}>
+            <div className={styles.playTopWrapper}>
+                <Button transparent>Wróć do biblioteki</Button>
+                {/* Tytuł utworu */}
+                <Button transparent onClick={addComment}>
+                    Dodaj komentarz
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export { CommentsSection };
