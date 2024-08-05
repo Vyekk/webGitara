f06669fcff4bbe433ade983573c65574b16fe491
Vyekk <koluchkon@gmail.com>
2024-08-05 04:02:06 +0200

    Added structure of comments section

--- src/components/CommentsSection/CommentsSection.tsx
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -1,8 +1,11 @@
 import Button from 'components/Button/Button';
 import styles from 'components/CommentsSection/CommentsSection.module.scss';
+import Title from 'components/Title/Title';
 
 type CommentsSectionProps = {
-    song: object;
+    songTitle: string;
+    author: string;
+    comments?: string[][];
 };
 
 const CommentsSection = (song: CommentsSectionProps) => {
@@ -11,13 +14,21 @@ const CommentsSection = (song: CommentsSectionProps) => {
     };
 
     return (
-        <div className={styles.commentsSectionWraper}>
-            <div className={styles.playTopWrapper}>
+        <div>
+            <div className={styles.commentsTopSectionWraper}>
                 <Button transparent>Wróć do biblioteki</Button>
-                {/* Tytuł utworu */}
+                {<Title tag="h3">{song.songTitle}</Title>}
                 <Button transparent onClick={addComment}>
                     Dodaj komentarz
                 </Button>
+            </div>
+            <div className={styles.commentsWrapper}>
+                {song.comments?.map((comment, index) => (
+                    <div key={index} className={styles.comment}>
+                        <div className={styles.commentText}>{comment[0]}</div>
+                        <div className={styles.commentAuthor}>{comment[1]}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
