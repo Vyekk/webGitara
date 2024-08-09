1d9d58c4843da1c8d33adce1a821573deebff0a8
Vyekk <koluchkon@gmail.com>
2024-08-09 05:11:22 +0200

    Added comment adding logic

--- src/components/CommentsSection/CommentsSection.tsx
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -1,7 +1,9 @@
 import Button from 'components/Button/Button';
 import styles from 'components/CommentsSection/CommentsSection.module.scss';
+import { MainMenu } from 'components/MainMenu/MainMenu';
+import { ModalContext } from 'components/Modal/ModalContext';
 import Title from 'components/Title/Title';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Song } from 'types';
 
 type CommentsSectionProps = {
@@ -10,24 +12,66 @@ type CommentsSectionProps = {
 
 const CommentsSection = ({ song }: CommentsSectionProps) => {
     const [commentedSong, setCommentedSong] = useState<Song>(song);
-    const addComment = () => {
-        console.log('Dodaj komentarz');
+    const [isCommenting, setIsCommenting] = useState(false);
+    const { openModal, setModal } = useContext(ModalContext);
+    const handleAddComment = () => {
+        setIsCommenting(true);
+    };
+
+    const handleBackToLibrary = () => {
+        setModal(<MainMenu />);
+        openModal();
+    };
+
+    const handleSendComment = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const target = e.target as HTMLFormElement;
         setCommentedSong((prev: Song) => ({
             ...prev,
-            comments: prev.comments ? [...prev.comments, ['Komentarz', 'Autor']] : [['Komentarz', 'Autor']],
+            comments: prev.comments
+                ? [...prev.comments, [target.commentContent.value, 'Autor']]
+                : [[target.commentContent.value, 'Autor']],
         }));
+        setIsCommenting(false);
     };
 
     return (
         <div>
             <div className={styles.commentsTopSectionWraper}>
-                <Button transparent>Wróć do biblioteki</Button>
+                <Button transparent onClick={handleBackToLibrary}>
+                    Wróć do biblioteki
+                </Button>
                 <Title tag="h3">{song.songTitle}</Title>
-                <Button transparent onClick={addComment}>
+                <Button transparent onClick={handleAddComment}>
                     Dodaj komentarz
                 </Button>
             </div>
             <div className={styles.commentsWrapper}>
+                {isCommenting && (
+                    <div className={styles.commentFormWrapper}>
+                        <form onSubmit={handleSendComment} className={styles.commentForm}>
+                            <Title tag="h4" orange>
+                                Dodaj komentarz
+                            </Title>
+                            <textarea
+                                maxLength={100}
+                                title="commmentContent"
+                                name="commentContent"
+                                className={styles.commentTextArea}
+                                placeholder="Wpisz komentarz (max 100 znaków)"
+                            ></textarea>
+                            <div className={styles.addCommentButtonsWrapper}>
+                                <Button type="submit">Wyślij</Button>
+                                <Button isDark onClick={() => setIsCommenting(false)}>
+                                    Anuluj
+                                </Button>
+                            </div>
+                        </form>
+                    </div>
+                )}
+                <Title tag="h4" orange>
+                    Komentarze
+                </Title>
                 {commentedSong.comments?.map((comment, index) => (
                     <div key={index} className={styles.comment}>
                         <div className={styles.commentText}>{comment[0]}</div>
