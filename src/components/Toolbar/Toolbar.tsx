ffc833f43b2a842498f71eb79482076b09d61c66
Vyekk <s20699@pjwstk.edu.pl>
2024-08-12 02:30:41 +0200

    Simplify to one function for DRY

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -3,8 +3,9 @@ import styles from 'components/Toolbar/Toolbar.module.scss';
 import playIcon from 'assets/play-icon.png';
 import settingsIcon from 'assets/settings-icon.png';
 import { ModalContext } from 'components/Modal/ModalContext';
-import { useContext } from 'react';
+import { ReactNode, useContext } from 'react';
 import { MainMenu } from 'components/MainMenu/MainMenu';
+import { Settings } from 'components/Settings/Settings';
 
 type ToolbarProps = {
     hasControls?: boolean;
@@ -14,26 +15,22 @@ type ToolbarProps = {
 const Toolbar = ({ hasControls }: ToolbarProps) => {
     const { openModal, setModal } = useContext(ModalContext);
 
-    const handlePlayButtonClick = () => {
-        const content = <MainMenu />;
-        setModal(content);
+    const handleOpenModal = (content: ReactNode) => {
+        const modalContent = content;
+        setModal(modalContent);
         openModal();
     };
-    const handleSettingsButtonClick = () => {
-        const content = <div>Settings</div>;
-        setModal(content);
-        openModal();
-    };
+
     return (
         <div className={styles.wrapper}>
             {hasControls && <Button isDark>BPM</Button>}
             <Button className={styles.secondOption} href="../new" transparent>
                 Nowy utwór
             </Button>
-            <Button className={styles.playButton} onClick={handlePlayButtonClick} circle>
+            <Button className={styles.playButton} onClick={() => handleOpenModal(<MainMenu />)} circle>
                 <img src={playIcon} alt="play" />
             </Button>
-            <Button className={styles.secondOption} onClick={handleSettingsButtonClick} transparent>
+            <Button className={styles.secondOption} onClick={() => handleOpenModal(<Settings />)} transparent>
                 <img className={styles.settingsIcon} src={settingsIcon} alt="settings" />
             </Button>
             <Button className={styles.secondOption} href="/login" transparent>
