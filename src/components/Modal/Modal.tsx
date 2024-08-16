53a62823482ea1a893272c8b7dda78e97f38e327
Vyekk <koluchkon@gmail.com>
2024-08-16 22:19:42 +0200

    Added max style for modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,11 +3,12 @@ import styles from 'components/Modal/Modal.module.scss';
 
 interface IModalProps {
     children: ReactNode;
+    isMax?: boolean;
     onClose?: () => void;
 }
 
-const Modal = ({ children, onClose }: IModalProps) => (
-    <div className={`${styles.wrapper} ${onClose ? styles.closeableWrapper : ''}`}>
+const Modal = ({ children, isMax, onClose }: IModalProps) => (
+    <div className={`${styles.wrapper} ${onClose ? styles.closeableWrapper : ''} ${isMax ? styles.max : ''}`}>
         {children}
         {onClose ? (
             <button className={styles.closeButton} onClick={onClose}>
