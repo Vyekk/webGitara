8249015fa14b0f0e67d9124c0fca0af090974c53
Vyekk <koluchkon@gmail.com>
2024-08-22 23:50:27 +0200

    Added default structure of settings menu

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,5 +1,13 @@
+import styles from 'components/Settings/Settings.module.scss';
+
 const Settings = () => {
-    return <div>Settings</div>;
+    return (
+        <div className={styles.settingsWrapper}>
+            <div className={styles.settingsNav}></div>
+            <div className={styles.settingsOne}></div>
+            <div className={styles.settingTwo}></div>
+        </div>
+    );
 };
 
 export { Settings };
