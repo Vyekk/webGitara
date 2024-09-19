e0242cdbbccaae2e6d6d0ddf1fd65150c0541cf7
Vyekk <koluchkon@gmail.com>
2024-09-19 23:53:05 +0200

    Added buttons settings menu

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,15 +1,35 @@
+import Button from 'components/Button/Button';
 import styles from 'components/Settings/Settings.module.scss';
 import Title from 'components/Title/Title';
+import { useState } from 'react';
 
 const Settings = () => {
+    const [settingsScreen, setSettingsScreen] = useState('myAccount');
+
     return (
         <div className={styles.settingsWrapper}>
             <div className={styles.settingsNav}>
                 <div className={styles.settingsLink}>
-                    <Title tag="h4">Moje konto</Title>
+                    <Button
+                        transparent
+                        isActive={settingsScreen === 'myAccount' ? true : false}
+                        onClick={() => {
+                            setSettingsScreen('myAccount');
+                        }}
+                    >
+                        Moje konto
+                    </Button>
                 </div>
                 <div className={styles.settingsLink}>
-                    <Title tag="h4">Ustawienia rozgrywki</Title>
+                    <Button
+                        transparent
+                        isActive={settingsScreen === 'playgroundSettings' ? true : false}
+                        onClick={() => {
+                            setSettingsScreen('playgroundSettings');
+                        }}
+                    >
+                        Ustawienia rozgrywki
+                    </Button>
                 </div>
             </div>
             <div className={styles.settingsOne}>
