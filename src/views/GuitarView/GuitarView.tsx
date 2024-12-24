74b043fb3f01e3e4a6fd3efe787f2b4accf67392
Vyekk <koluchkon@gmail.com>
2024-12-24 03:57:59 +0100

    Added song controls and security path

--- src/views/GuitarView/GuitarView.tsx
+++ src/views/GuitarView/GuitarView.tsx
@@ -3,13 +3,19 @@ import Toolbar from 'components/Toolbar/Toolbar';
 import { useEffect, useRef, useState } from 'react';
 import { Song } from 'types';
 import styles from 'views/GuitarView/GuitarView.module.scss';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Button from 'components/Button/Button';
+import Slider from 'components/Slider/Slider';
+import { Link } from 'react-router-dom';
 
 const GuitarView = () => {
-    const [song, setSong] = useState<Song | null>(null);
+    const songsData = localStorage.getItem('songs');
+    const songs = songsData ? JSON.parse(songsData) : [];
+    const [song, setSong] = useState<Song>(songs[0]);
     const fretboardRef = useRef<HTMLDivElement | null>(null);
     const [currentUrl, setCurrentUrl] = useState(window.location.href);
     const location = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         setupSong();
@@ -20,12 +26,13 @@ const GuitarView = () => {
     }, [location]);
 
     const setupSong = () => {
-        // Pobierz utwór z localStorage async
         const fetchSong = async () => {
-            const songsData = localStorage.getItem('songs');
-            const songs = songsData ? JSON.parse(songsData) : [];
             const songId = currentUrl.split('/').pop();
             const song = songs.find((song: Song) => song.id === Number(songId));
+            if (!song) {
+                navigate('/play/guitar/1');
+                return;
+            }
             setSong(song);
         };
         fetchSong();
@@ -57,16 +64,20 @@ const GuitarView = () => {
 
     return (
         <div>
+            <div className={styles.linkWrapper}>
+                <Link to="/play/dashboard"> &lt; powrót do dashboard</Link>
+            </div>
             <div className={styles.wrapper}>
                 <Title>{`${song?.songTitle} - ${song?.author}`}</Title>
             </div>
             <Toolbar />
             <div className={styles.fretboard} ref={fretboardRef}></div>
+            <Slider max={song.tabulature.length} />
             <div className={styles.songControl}>
-                <button className={styles.goBackButton} title="Cofnij"></button>
-                <button className={styles.playButton} title="Start"></button>
-                <button className={styles.stopButton} title="Stop"></button>
-                <button className={styles.forwardButton} title="Naprzód"></button>
+                <Button className={styles.goBackButton} transparent title="Cofnij"></Button>
+                <Button className={styles.playButton} transparent title="Odtwórz"></Button>
+                <Button className={styles.stopButton} transparent title="Stop"></Button>
+                <Button className={styles.forwardButton} transparent title="Naprzód"></Button>
             </div>
         </div>
     );
