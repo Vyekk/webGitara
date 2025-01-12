6cdd09a5efa41bcfa8a288871f004c51326e7aa9
Vyekk <koluchkon@gmail.com>
2025-01-12 02:22:50 +0100

    Changed name function to be more accurately and repair path to form component

--- src/views/LoginView/LoginView.tsx
+++ src/views/LoginView/LoginView.tsx
@@ -6,11 +6,11 @@ import Section from 'components/Section/Section';
 import loginPageImage from 'assets/login-page-background.png';
 import styles from 'views/LoginView/LoginView.module.scss';
 import Modal from 'components/Modal/Modal';
-import Form from 'components/Form/Form';
+import AuthForm from 'components/Form/AuthForm';
 
 const LoginView = () => {
     const navigate = useNavigate();
-    const loadUser = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         navigate('/play/dashboard');
     };
@@ -27,7 +27,7 @@ const LoginView = () => {
                 <Logo />
                 <Link to="/">&lt; Wróć do strony głównej</Link>
                 <Modal>
-                    <Form submitFn={loadUser} />
+                    <AuthForm submitFn={handleLogin} />
                 </Modal>
             </div>
         </Section>
