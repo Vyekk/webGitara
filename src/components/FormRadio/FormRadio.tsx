e2ed06e0ea30b9b6ef2cfa8942f28bbc0b7fd767
Vyekk <koluchkon@gmail.com>
2025-01-12 02:25:07 +0100

    Repaired path to scss file

--- src/components/FormRadio/FormRadio.tsx
+++ src/components/FormRadio/FormRadio.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from 'components/Form/FormRadio.module.scss';
+import styles from 'components/FormRadio/FormRadio.module.scss';
 
 interface IRadioProps {
     name: string;
