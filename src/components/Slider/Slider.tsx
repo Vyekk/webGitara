780a8b50432743e5f3740e32b80e8eeebc365d64
Vyekk <koluchkon@gmail.com>
2025-04-09 00:49:37 +0200

    Changed slider to set current step proper

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -10,15 +10,17 @@ const Slider = ({ max, onChange }: ISliderProps) => {
     const [value, setValue] = useState(1); // Początkowa wartość slidera
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(Number(event.target.value));
-        onChange(value);
+        const newValue = Number(event.target.value);
+        setValue(newValue);
+        console.log('wartość value:', newValue - 1);
+        onChange(newValue - 1);
     };
 
     return (
         <div className={styles.slider}>
             <label>
                 Krok: {value}
-                <input type="range" min="1" max={max + 1} value={value} onChange={handleChange} />
+                <input type="range" min="1" max={max} value={value} onChange={handleChange} />
             </label>
         </div>
     );
