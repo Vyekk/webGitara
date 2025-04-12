634ad943cdf3b399ca90896751bc9b251c2125b9
Vyekk <koluchkon@gmail.com>
2025-04-12 04:06:12 +0200

    Added option to close modal by clicking out of the div

--- src/views/PlayView/PlayView.tsx
+++ src/views/PlayView/PlayView.tsx
@@ -1,21 +1,42 @@
+import { useEffect, useContext, useRef } from 'react';
 import Section from 'components/Section/Section';
 import styles from 'views/PlayView/PlayView.module.scss';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import DashboardView from 'views/DashboardView/DashboardView';
 import { GuitarView } from 'views/GuitarView/GuitarView';
 import Modal from 'components/Modal/Modal';
-import { useContext } from 'react';
 import { ModalContext } from 'components/Modal/ModalContext';
 
-const PlayView = () => {
+const PlayView: React.FC = () => {
     const { isModalOpen, closeModal, modalContent } = useContext(ModalContext);
+    const modalRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (!isModalOpen || !closeModal) return;
+
+        const handleClickOutside = (event: MouseEvent) => {
+            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                closeModal();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isModalOpen, closeModal]);
+
     return (
         <Section id={styles.playBackground}>
-            {isModalOpen && <Modal onClose={closeModal}>{modalContent}</Modal>}
+            {isModalOpen && (
+                <Modal ref={modalRef} onClose={closeModal}>
+                    {modalContent}
+                </Modal>
+            )}
             <Routes>
-                <Route path="dashboard" element={<DashboardView />}></Route>
+                <Route path="dashboard" element={<DashboardView />} />
                 <Route path="guitar" element={<Navigate to="1" replace />} />
-                <Route path="guitar/:id" element={<GuitarView />}></Route>
+                <Route path="guitar/:id" element={<GuitarView />} />
             </Routes>
         </Section>
     );
