db0c53463f14e8abb3df27b3ea56f434b266ba8f
Vyekk <koluchkon@gmail.com>
2025-04-15 03:29:35 +0200

    Deleted strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'index.scss';
 import App from 'views/App/App';
 import './utils/store_songs_to_localstorage';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
