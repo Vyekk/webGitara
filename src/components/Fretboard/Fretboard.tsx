0c8ec854cb9b581ff47dda6438b386779d612a8e
Vyekk <koluchkon@gmail.com>
2025-05-01 03:41:44 +0200

    Added notes

--- src/components/Fretboard/Fretboard.tsx
+++ src/components/Fretboard/Fretboard.tsx
@@ -10,6 +10,8 @@ interface FretboardProps {
 const Fretboard: React.FC<FretboardProps> = ({ numberOfStrings, numberOfFrets, notesToShow }) => {
     const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
     const doubleFretMarkPositions = [12, 24];
+    const instrumentTuning = [4, 11, 7, 2, 9, 4];
+    const notesSharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
     useEffect(() => {
         showNotes(notesToShow || '');
@@ -19,6 +21,12 @@ const Fretboard: React.FC<FretboardProps> = ({ numberOfStrings, numberOfFrets, n
         console.log(info);
     };
 
+    const generateNoteNames = (noteIndex: number) => {
+        noteIndex = noteIndex % 12;
+        const noteName = notesSharp[noteIndex];
+        return noteName;
+    };
+
     return (
         <div className={styles.fretboard}>
             {Array.from({ length: numberOfStrings }, (_, stringIndex) => (
@@ -26,14 +34,14 @@ const Fretboard: React.FC<FretboardProps> = ({ numberOfStrings, numberOfFrets, n
                     {Array.from({ length: numberOfFrets + 1 }, (_, fretIndex) => (
                         <div
                             key={fretIndex}
-                            className={`${styles.noteFret} ${
-                                singleFretMarkPositions.includes(fretIndex) && stringIndex === 0
-                                    ? styles.singleFretmark
-                                    : ''
-                            }`}
+                            className={styles.noteFret}
                             data-string={stringIndex + 1}
                             data-fret={fretIndex}
+                            data-note={generateNoteNames(fretIndex + instrumentTuning[stringIndex])}
                         >
+                            {singleFretMarkPositions.includes(fretIndex) && stringIndex === 0 && (
+                                <div className={styles.singleFretmark}></div>
+                            )}
                             {doubleFretMarkPositions.includes(fretIndex) && stringIndex === 0 && (
                                 <div className={styles.doubleFretmark}></div>
                             )}
