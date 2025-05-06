4eaa87378c7efe3791b45a6037b5b64e64a75d8d
Vyekk <koluchkon@gmail.com>
2025-05-06 03:58:08 +0200

    Changed strings numbers (1 - is e1 and 6 is e6)

--- src/components/Fretboard/Fretboard.tsx
+++ src/components/Fretboard/Fretboard.tsx
@@ -16,9 +16,9 @@ interface FretboardProps {
 const Fretboard: React.FC<FretboardProps> = ({ numberOfStrings, numberOfFrets, notesToShow, isReversed }) => {
     const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
     const doubleFretMarkPositions = [12, 24];
-    const instrumentTuning = [4, 9, 2, 7, 11, 4];
+    const instrumentTuning = [4, 11, 7, 2, 9, 4];
     const notesSharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-    const stringNames = ['E', 'A', 'D', 'G', 'B', 'E'];
+    const stringNames = ['E', 'B', 'G', 'D', 'A', 'E'];
 
     useEffect(() => {
         showStringsNames();
@@ -104,8 +104,8 @@ const Fretboard: React.FC<FretboardProps> = ({ numberOfStrings, numberOfFrets, n
 
     return (
         <div className={styles.fretboard}>
-            {Array.from({ length: numberOfStrings }, (_, stringIndex) => stringIndex)
-                .map((i) => (isReversed ? numberOfStrings - 1 - i : i))
+            {Array.from({ length: numberOfStrings }, (_, i) => i)
+                .map((i) => (isReversed ? i : numberOfStrings - 1 - i))
                 .map((stringIndex) => (
                     <div key={stringIndex} className={styles.string} data-string={stringIndex + 1}>
                         {Array.from({ length: numberOfFrets + 1 }, (_, fretIndex) => (
