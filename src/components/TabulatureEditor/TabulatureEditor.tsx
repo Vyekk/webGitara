b9fe3993fd6431f12d541bbf29209d5fd38209a6
Vyekk <koluchkon@gmail.com>
2025-05-13 23:02:02 +0200

    Added TabulatureEditor files

---
+++ src/components/TabulatureEditor/TabulatureEditor.tsx
@@ -0,0 +1,7 @@
+import styles from './TabulatureEditor.module.scss';
+
+const TabulatureEditor = () => {
+    return <div className={styles.tabulatureEditorWrapper}></div>;
+};
+
+export default TabulatureEditor;
