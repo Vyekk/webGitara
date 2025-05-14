6370c32fd1a2803e70fb9b702ab5bb3250770056
Vyekk <koluchkon@gmail.com>
2025-05-14 23:51:12 +0200

    Added files for Tabulature editor view

---
+++ src/views/tabulatureEditorView/TabulatureEditorView.tsx
@@ -0,0 +1,12 @@
+import styles from './views/TabulatureEditorView/TabulatureEditorView.module.scss';
+
+const TabulatureEditorView = () => {
+    return (
+        <div className={styles.tabulatureEditorView}>
+            <h1>Tabulature Editor</h1>
+            <p>This is the tabulature editor view.</p>
+        </div>
+    );
+};
+
+export default TabulatureEditorView;
