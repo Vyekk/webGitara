a5cd83554a991262e5be3b2e11653f5262789348
Vyekk <koluchkon@gmail.com>
2025-05-17 15:54:35 +0200

    Added simple form to add song

--- src/views/tabulatureEditorView/TabulatureEditorView.tsx
+++ src/views/tabulatureEditorView/TabulatureEditorView.tsx
@@ -1,12 +1,55 @@
-import styles from './views/TabulatureEditorView/TabulatureEditorView.module.scss';
+import { useParams } from 'react-router-dom';
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
+import TabulatureEditor from 'components/TabulatureEditor/TabulatureEditor';
+import Title from 'components/Title/Title';
+import styles from 'views/TabulatureEditorView/TabulatureEditorView.module.scss';
+import { useEffect, useState } from 'react';
+import { Song } from 'types';
 
 const TabulatureEditorView = () => {
+    const { id } = useParams();
+    const [song, setSong] = useState<Song | null>(null);
+    const songsData = localStorage.getItem('songs');
+    const songs = songsData ? JSON.parse(songsData) : [];
+
+    useEffect(() => {
+        setupSong();
+    }, [id]);
+
+    useEffect(() => {
+        if (!song) return;
+        console.log('Song:', song);
+    }, [song]);
+
+    const setupSong = () => {
+        const fetchSong = async () => {
+            const song = songs.find((song: Song) => song.id === Number(id));
+            if (!song) {
+                console.error('Song not found');
+                return;
+            }
+            setSong(song);
+        };
+        fetchSong();
+    };
     return (
-        <div className={styles.tabulatureEditorView}>
-            <h1>Tabulature Editor</h1>
-            <p>This is the tabulature editor view.</p>
+        <div className={styles.tabulatureEditorViewWrapper}>
+            <div className={styles.textContentWrapper}>
+                <Title>Edycja utworu {song ? `"${song.songTitle}"` : ''}</Title>
+                <form>
+                    <TabulatureEditor />
+                    <Input id="songName" value={song ? song.songTitle : ''}>
+                        Nazwa utworu
+                    </Input>
+                    <Input id="authorName" readOnly>
+                        {song ? song.author : ''}
+                    </Input>
+                    <Button>Zapisz utwór</Button>
+                </form>
+            </div>
         </div>
     );
 };
 
-export default TabulatureEditorView;
+export { TabulatureEditorView };
