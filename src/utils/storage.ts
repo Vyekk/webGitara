0390186cf0ed15fc3fe4280325e5d706dde973b7
Vyekk <koluchkon@gmail.com>
2025-05-19 01:33:24 +0200

    Added temporary local storage functions

---
+++ src/utils/storage.ts
@@ -0,0 +1,26 @@
+import { Song } from 'types';
+
+const STORAGE_KEYS = {
+    SONGS: 'songs',
+    IS_FRETBOARD_REVERSED: 'isFretboardReversed',
+};
+
+// SONGS
+export async function loadSongs() {
+    const raw = localStorage.getItem(STORAGE_KEYS.SONGS);
+    return raw ? JSON.parse(raw) : [];
+}
+
+export async function saveSongs(songs: Song[]) {
+    localStorage.setItem(STORAGE_KEYS.SONGS, JSON.stringify(songs));
+}
+
+// IS_FRETBOARD_REVERSED
+export async function loadIsFretboardReversed() {
+    const raw = localStorage.getItem(STORAGE_KEYS.IS_FRETBOARD_REVERSED);
+    return raw ? JSON.parse(raw) : false;
+}
+
+export async function saveIsFretboardReversed(value: boolean) {
+    localStorage.setItem(STORAGE_KEYS.IS_FRETBOARD_REVERSED, JSON.stringify(value));
+}
