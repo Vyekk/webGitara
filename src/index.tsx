7eb525244ad31368c479fe2cdf75df8461c730e8
Vyekk <koluchkon@gmail.com>
2025-05-21 00:58:30 +0200

    Changed to more readable version

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,9 @@ import 'index.scss';
 import App from 'views/App/App';
 import './utils/store_songs_to_localstorage';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(<App />);
+} else {
+    throw new Error('Root element not found');
+}
