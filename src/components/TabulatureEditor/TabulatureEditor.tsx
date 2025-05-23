839d2ae0dcb4e241eb69a94e17347d04bbb5ec6f
Vyekk <koluchkon@gmail.com>
2025-05-23 23:48:28 +0200

    Added structure for tabulatureEditor

--- src/components/TabulatureEditor/TabulatureEditor.tsx
+++ src/components/TabulatureEditor/TabulatureEditor.tsx
@@ -1,7 +1,29 @@
 import styles from 'components/TabulatureEditor/TabulatureEditor.module.scss';
 
-const TabulatureEditor = () => {
-    return <div className={styles.tabulatureEditorWrapper}></div>;
+interface TabulatureEditorProps {
+    numberOfStrings: number;
+    isReversed?: boolean;
+}
+
+const TabulatureEditor: React.FC<TabulatureEditorProps> = ({ numberOfStrings, isReversed }) => {
+    return (
+        <div className={styles.tabulatureEditorWrapper}>
+            {Array.from({ length: numberOfStrings }, (_, i) => i)
+                .map((i) => (isReversed ? i : numberOfStrings - 1 - i))
+                .map((stringIndex) => (
+                    <div key={stringIndex} className={styles.string} data-string={stringIndex + 1}>
+                        {Array.from({ length: 50 }, (_, tabColumnIndex) => (
+                            <div
+                                key={tabColumnIndex}
+                                className={styles.noteFret}
+                                data-string={stringIndex + 1}
+                                data-tabColumn={tabColumnIndex}
+                            ></div>
+                        ))}
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default TabulatureEditor;
