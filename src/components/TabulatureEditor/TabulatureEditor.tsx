4b64bf3df1b7dc887f4c38c29af9a400ac571019
Vyekk <koluchkon@gmail.com>
2025-05-26 23:08:39 +0200

    Added strings labels

--- src/components/TabulatureEditor/TabulatureEditor.tsx
+++ src/components/TabulatureEditor/TabulatureEditor.tsx
@@ -6,12 +6,17 @@ interface TabulatureEditorProps {
 }
 
 const TabulatureEditor: React.FC<TabulatureEditorProps> = ({ numberOfStrings, isReversed }) => {
+    const stringsLabels = ['E', 'A', 'D', 'G', 'B', 'E'];
     return (
         <div className={styles.tabulatureEditorWrapper}>
             {Array.from({ length: numberOfStrings }, (_, i) => i)
                 .map((i) => (isReversed ? i : numberOfStrings - 1 - i))
                 .map((stringIndex) => (
                     <div key={stringIndex} className={styles.string} data-string={stringIndex + 1}>
+                        <span key={stringIndex + stringsLabels[stringIndex]} className={styles.stringLabel}>
+                            {stringsLabels[stringIndex]}
+                        </span>
+
                         {Array.from({ length: 50 }, (_, tabColumnIndex) => (
                             <div
                                 key={tabColumnIndex}
