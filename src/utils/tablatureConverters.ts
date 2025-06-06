5555a1172eae4ee1ef6547a3bb59e38fd4f86f4e
Vyekk <koluchkon@gmail.com>
2025-06-06 03:17:46 +0200

    Added tablature line support

--- src/utils/tablatureConverters.ts
+++ src/utils/tablatureConverters.ts
@@ -68,15 +68,19 @@ export const convertFormDataToTablature = (formData: FormDataMap, formDataDurati
 
 export const convertTablatureToFormData = (
     tablature: Tablature,
-    lineNumber = 0,
 ): { formData: FormDataMap; formDataDuration: FormDataMap } => {
     const formData: FormDataMap = {};
     const formDataDuration: FormDataMap = {};
-
-    tablature.forEach((step: TabNote[], columnIndex: number) => {
+    let lineNumber = 1;
+    let columnIndex = 1;
+    tablature.forEach((step: TabNote[]) => {
         step.forEach((note) => {
-            const col = `column-${columnIndex + 1}`;
-            const line = `line-${lineNumber + 1}`;
+            if (columnIndex > 50) {
+                lineNumber++;
+                columnIndex = 0;
+            }
+            const col = `column-${columnIndex}`;
+            const line = `line-${lineNumber}`;
             if (!('rest' in note && note.rest)) {
                 const key = `string-${note.guitarString}-${col}-${line}`;
                 const value = `${note.guitarFret}`;
@@ -84,38 +88,12 @@ export const convertTablatureToFormData = (
             }
             const durationSymbol = getSymbolFromDurationValue(note.duration);
             if (durationSymbol) {
-                const durationKey = `duration-${lineNumber + 1}-${columnIndex + 1}`;
+                const durationKey = `duration-${lineNumber}-${columnIndex}`;
                 formDataDuration[durationKey] = durationSymbol;
             }
         });
+        columnIndex++;
     });
 
     return { formData, formDataDuration };
 };
-
-// tablature.forEach((step, columnIndex) => {
-//     if (step.length === 1 && step[0].rest) {
-//         // Pauza – pusta kolumna
-//         for (let stringIndex = 0; stringIndex < numberOfStrings; stringIndex++) {
-//             const key = `string-${stringIndex}-column-${columnIndex}-line-0`;
-//             formData[key] = '';
-//         }
-//         const durationSymbol = getSymbolFromDurationValue(step[0].duration);
-//         if (durationSymbol) {
-//             formDataDuration[`duration-0-${columnIndex}`] = durationSymbol;
-//         }
-//     } else {
-//         // Normalne nuty
-//         step.forEach((note) => {
-//             const stringIndex = note.guitarString !== undefined ? numberOfStrings - 1 - note.guitarString : 0;
-//             const key = `string-${stringIndex}-column-${columnIndex}-line-0`;
-//             formData[key] = String(note.guitarFret);
-
-//             // Zapisujemy duration tylko raz na kolumnę
-//             const durationSymbol = getSymbolFromDurationValue(note.duration);
-//             if (durationSymbol) {
-//                 formDataDuration[`duration-0-${columnIndex}`] = durationSymbol;
-//             }
-//         });
-//     }
-// });
