471cdfca65a2bd862ca2e3810c26e759633c5ddb
Vyekk <koluchkon@gmail.com>
2025-06-09 02:57:48 +0200

    Added checking if user logged

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,5 +1,10 @@
-const PrivateRoute = () => {
-    return <div>PrivateRoute</div>;
-};
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+
+function PrivateRoute() {
+    const { isLoggedIn, isAuthLoaded } = useAuth();
+    if (!isAuthLoaded) return null;
+    return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+}
 
 export default PrivateRoute;
