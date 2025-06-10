321eab08f47b05856d1cfe1daa2e4e1f9f178bfd
Vyekk <koluchkon@gmail.com>
2025-06-11 00:09:58 +0200

    Added filtering for songs (favourties, user's songs)

--- src/components/SongsLibrary/SongsLibrary.tsx
+++ src/components/SongsLibrary/SongsLibrary.tsx
@@ -1,26 +1,59 @@
 import Button from 'components/Button/Button';
 import styles from './SongsLibrary.module.scss';
 import { SongsList } from 'components/SongsList/SongsList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Input from 'components/Input/Input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useSongs } from 'context/SongsContext';
+import useRequiredUser from 'utils/useRequiredUser';
+import { useAuth } from 'context/AuthContext';
+import { Song } from 'types';
 
 const SongsLibrary = () => {
     const [buttonType, setButtonType] = useState<'allSongs' | 'mySongs'>('allSongs');
     const [searchTerm, setSearchTerm] = useState('');
+    const [filteredSongs, setFilteredSongs] = useState<Song[]>([]);
     const [isShowingFavourites, setIsShowingFavourites] = useState(false);
+    const { isFavourite } = useAuth();
 
     const { songs } = useSongs();
-
+    const user = useRequiredUser();
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event.target.value);
     };
 
     const handleShowFavorites = () => {
         setIsShowingFavourites((prev) => !prev);
+        if (!isShowingFavourites) {
+            let newFilteredSongs: Song[] = [];
+            if (buttonType === 'mySongs' && user) {
+                newFilteredSongs = filteredSongs.filter((song) => {
+                    return isFavourite(song.id);
+                });
+            } else {
+                newFilteredSongs = songs.filter((song) => {
+                    return isFavourite(song.id);
+                });
+            }
+            setFilteredSongs(newFilteredSongs);
+        } else {
+            showSongs();
+        }
     };
+
+    const showSongs = () => {
+        if (user && buttonType === 'mySongs') {
+            const newFilteredSongs = songs.filter((song) => song.idUser === user.idUser);
+            setFilteredSongs(newFilteredSongs);
+        } else {
+            setFilteredSongs(songs);
+        }
+    };
+
+    useEffect(() => {
+        showSongs();
+    }, [buttonType]);
 
     return (
         <div className={styles.songsLibraryWrapper}>
@@ -46,11 +79,7 @@ const SongsLibrary = () => {
                 </Button>
             </div>
             <div className={styles.songsWrapper}>
-                {buttonType === 'allSongs' ? (
-                    <SongsList songs={songs} searchTerm={searchTerm} isShowingFavourites={isShowingFavourites} />
-                ) : (
-                    <SongsList songs={[]} searchTerm={searchTerm} />
-                )}
+                <SongsList songs={filteredSongs} searchTerm={searchTerm} isShowingFavourites={isShowingFavourites} />
             </div>
         </div>
     );
