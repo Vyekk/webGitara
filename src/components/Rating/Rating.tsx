c8d67db01bcfb0db621d4540a3a507933233319b
Vyekk <koluchkon@gmail.com>
2025-06-12 04:12:29 +0200

    Changed rating for database

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,32 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Star } from 'components/Star/Star';
 import styles from './Rating.module.scss';
-import { useState } from 'react';
+import { Song } from 'types';
+import useRequiredUser from 'utils/useRequiredUser';
+import { useSongs } from 'context/SongsContext';
 
-const Rating = ({ rating = [], isHover }: { rating?: number[]; isHover: boolean }) => {
-    const [userRating, setUserRating] = useState(0);
+const Rating = ({ song, isHover }: { song: Song; isHover: boolean }) => {
+    const user = useRequiredUser();
+    const { rateSong } = useSongs();
+    const userId = user.idUser;
 
-    const calculateRating = (rating: number[]) => {
-        return rating.reduce((a, b) => a + b, 0) / (rating.length || 1);
-    };
+    const getUserRating = () => song.rating.find((r) => r.userId === userId)?.value || 0;
 
-    const [socialRating, setSocialRating] = useState(calculateRating(rating));
+    const calculateAverage = () => song.rating.reduce((sum, r) => sum + r.value, 0) / (song.rating.length || 1);
 
-    const rateSong = (rate: number) => {
-        setUserRating(rate);
-        setSocialRating(calculateRating([...rating, rate]));
+    const [userRating, setUserRating] = useState(getUserRating());
+    const [average, setAverage] = useState(calculateAverage());
+
+    useEffect(() => {
+        setUserRating(getUserRating());
+        setAverage(calculateAverage());
+    }, [song]);
+
+    const handleRateSong = async (rate: number) => {
+        if (!userId) return;
+
+        try {
+            await rateSong(song.id, rate);
+        } catch (error) {
+            console.error('Błąd przy ocenianiu:', error);
+        }
     };
 
     return (
         <div className={styles.rating}>
             {!isHover &&
                 Array.from({ length: 5 }, (_, i) => (
-                    <Star key={i} fill={i + 1 <= socialRating ? 'full' : 'none'} rating={i + 1} rateSong={rateSong} />
+                    <Star key={i} fill={i + 1 <= average ? 'full' : 'none'} rating={i + 1} rateSong={handleRateSong} />
                 ))}
             {isHover &&
                 Array.from({ length: 5 }, (_, i) => (
-                    <Star key={i} fill={i + 1 <= userRating ? 'full' : 'none'} rating={i + 1} rateSong={rateSong} />
+                    <Star
+                        key={i}
+                        fill={i + 1 <= userRating ? 'full' : 'none'}
+                        rating={i + 1}
+                        rateSong={handleRateSong}
+                    />
                 ))}
-            <div className={styles.ratingNumber}>({socialRating.toFixed(1)})</div>
+            <div className={styles.ratingNumber}>({average.toFixed(1)})</div>
         </div>
     );
 };
