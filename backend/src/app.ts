2d1a369b0609e9c8a3d053fec38c191fea1d7ddc
Vyekk <koluchkon@gmail.com>
2025-06-21 16:19:17 +0200

    Changed name of server.ts to app.ts and changed for passenger

---
+++ backend/src/app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import 'dotenv/config';
+import { db } from './db';
+
+declare const PhusionPassenger: any;
+
+if (typeof PhusionPassenger !== 'undefined') {
+    PhusionPassenger.configure({ autoInstall: false });
+}
+
+import userRoutes from './routes/users';
+import songsRoutes from './routes/songs';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/songs', songsRoutes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+(async () => {
+    try {
+        const [rows] = await db.query('SELECT 1');
+        console.log('✅ Połączono z bazą danych!');
+    } catch (err) {
+        console.error('❌ Błąd połączenia z bazą danych:', err);
+    }
+})();
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+if (typeof PhusionPassenger !== 'undefined') {
+    app.listen('passenger');
+} else {
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+}
+
+// const PORT = 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
