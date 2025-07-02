0b38c31dfb02ee57bb5cc7fd6aa0afdf25721392
Vyekk <koluchkon@gmail.com>
2025-07-02 16:46:38 +0200

    Changed url for localhost

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://konradkoluch.usermd.net' || 'http://localhost:5000';
+const API_URL = 'http://localhost:5000';
+//process.env.REACT_APP_API_URL || 'https://konradkoluch.usermd.net' ||
 export default API_URL;
