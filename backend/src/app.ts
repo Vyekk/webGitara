2982f88931f1a9980089d3764ef916ba08fc6d40
Vyekk <koluchkon@gmail.com>
2025-07-04 14:42:08 +0200

    Added node-cron and connection for passenger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import path from 'path';
 import 'dotenv/config';
 import { db } from './db';
+import cron from 'node-cron';
 
 declare const PhusionPassenger: any;
 
@@ -14,7 +15,6 @@ import userRoutes from './routes/users';
 import songsRoutes from './routes/songs';
 
 const app = express();
-app.disable('x-powered-by');
 
 app.use(cors());
 app.use(express.json());
@@ -24,21 +24,27 @@ app.use('/api/songs', songsRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-(async () => {
-    try {
-        const [rows] = await db.query('SELECT 1');
-        console.log('✅ Połączono z bazą danych!');
-    } catch (err) {
-        console.error('❌ Błąd połączenia z bazą danych:', err);
-    }
-})();
-
 app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = 5000;
+if (typeof PhusionPassenger !== 'undefined') {
+    app.listen('passenger');
+} else {
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+cron.schedule('0 */2 * * *', async () => {
+    try {
+        console.log('Usuwanie usuniętych utworów z kosza...');
+        await db.query('DELETE FROM songs WHERE deleted_by_idUser IS NOT NULL');
+        console.log('Usunięte utwory zostały usunięte.');
+    } catch (err) {
+        console.error('Błąd podczas usuwania utworów z kosza:', err);
+    }
 });
+
+export default app;
