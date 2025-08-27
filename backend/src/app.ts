5358fca4b41a20322028db7705bf6cfead947e6d
Vyekk <koluchkon@gmail.com>
2025-08-28 01:06:23 +0200

    Added limit for json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,7 @@ app.use(
         credentials: true,
     }),
 );
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
@@ -43,11 +43,11 @@ app.use(express.static(path.join(__dirname, '../../public')));
 
 cron.schedule('0 */2 * * *', async () => {
     try {
-        console.log('Usuwanie usuniętych utworów z kosza...');
+        console.log('Czyszczenie usuniętych utworów z kosza...');
         await db.query('DELETE FROM songs WHERE deleted_by_idUser IS NOT NULL');
-        console.log('Usunięte utwory zostały usunięte.');
+        console.log('Usunięte utwory zostały wyczyszczone.');
     } catch (err) {
-        console.error('Błąd podczas usuwania utworów z kosza:', err);
+        console.error('Błąd podczas czyszczenia utworów z kosza:', err);
     }
 });
 
