18e972ae29f71285988e3cddd87480004c27bba3
Vyekk <koluchkon@gmail.com>
2025-09-06 21:46:38 +0200

    Deleted unused file

--- src/components/RequireAuth/RequireAuth.tsx
+++
@@ -1,8 +0,0 @@
-// import { Navigate } from 'react-router-dom';
-// import { useAuth } from '@/context/AuthContext';
-
-export const RequireAuth = ({ children }: { children: React.ReactNode }) => {
-    // const { user } = useAuth();
-    // if (!user) return <Navigate to="/login" />;
-    return <>{children}</>;
-};
